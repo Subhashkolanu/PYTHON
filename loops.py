4243098428199e0af12c50183583359cbd800e6d
Subhash <kos720500@gmail.com>
2026-01-25 12:59:10 +0530

    loops

--- loops.py
+++ loops.py
@@ -1,8 +1,40 @@
-a = "vvitu"
-i=0
-j=0
-while(i<5):
-    while(j<=i):
-        print(a[j])
-        j=j+1
-    i=i+1
+for row in range(3):
+    for col in range(4):
+        print("VVITU", end=" ")
+    print()
+print("New")
+n = 4
+for i in range(1,n+1):
+    for j in range(i):
+        print("VVITU",end = " ")
+    print()
+print("New")
+n = 5
+for i in range(n,0,-1):
+    for j in range(i):
+        print("VVITU",end = " ")
+    print()
+print("New")
+n = 3
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end = " ")
+    print()
+print("New")
+n = 4
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end = " ")
+    print()
+    print("New")
+n = 4
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("",end = " ")
+    print()
+print("New")
+word = "VVITU"
+for i in range(1,len(word)+1):
+    for j in range(i):
+        print(word[j],end = " ")
+    print()
