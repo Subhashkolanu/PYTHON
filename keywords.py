92ffaa207853b79cc0dca072e9516cdc74b462e0
Subhash <kos720500@gmail.com>
2026-01-28 12:04:27 +0530

    simple_interest

--- keywords.py
+++ keywords.py
@@ -1,3 +1,7 @@
 help("keywords")
 import keyword
 print(keyword.kwlist)
+test = keyword.iskeyword('elif')
+length = len(keyword.kwlist)
+print(test)
+print(length)
--- simple_interest.py
+++ simple_interest.py
@@ -1,7 +1,8 @@
-principal_amt = int(input("Enter the principal ammount : "))
-time_taken = int(input("Enter time taken in years : "))
+principal_amt = int(input("Enter the principal amount : "))
 interest_rate = int(input("Enter the interest rate : "))
+time_taken = int(input("Enter time taken in months : "))
 simple_interest = (principal_amt*time_taken*interest_rate)/100
 total_amount = simple_interest+principal_amt
-print("SIMPLE INTEREST : ",simple_interest)
-print("Total amount : ",total_amount)
+print("SIMPLE INTEREST for Every month: ",(principal_amt*interest_rate)/100)
+print("SIMPLE INTEREST for the time: ",simple_interest)
+print("Total amount to be repayed : ",total_amount)
