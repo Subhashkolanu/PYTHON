175a58730308eeaf655b8feccef8f660dd423e2d
Subhash <kos720500@gmail.com>
2026-01-29 20:18:07 +0530

    Right angled triangle

--- Right angled triangle.py
+++ Right angled triangle.py
@@ -1,4 +1,4 @@
-for i in range(1,6):
-    for j in range(i):
-        print("*",end=" ")
-        print( )
+for row in range(1,6):
+    for col in range(row):
+        print("*", end="")
+    print()
