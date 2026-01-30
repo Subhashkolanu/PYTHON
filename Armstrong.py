3d8653706dcf1eddc7f44395536f4259693081e3
Subhash <kos720500@gmail.com>
2026-01-30 15:17:38 +0530

    Armstrong

--- Armstrong.py
+++ Armstrong.py
@@ -4,9 +4,9 @@ print(m)
 a=0
 while n!=0:
     rem = n%10
-    a = a+rem*rem*rem
-    n = n/10
-if int(a)==int(m):
+    a = a+rem**3
+    n = n//10
+if a==m:
     print("Armstrong Number")
 else:
     print("Not a Armstrong number")
