a52cb1f45c4983254ebef31e26ab5a274e59f714
Subhash <kos720500@gmail.com>
2026-01-31 20:38:06 +0530

    parking ticket

--- parking ticket.py
+++ parking ticket.py
@@ -1,4 +1,5 @@
-veh = input('''Enter the vehicle type :
+import random
+veh = input('''choose the vehicle type :
             A.Two wheeler
             B.car
             C.Truck/Bus(Heavy vehicle) ''')
@@ -6,16 +7,17 @@ if veh == "A":
     reg_no=input("Enter the Reg no : ")
     time=int(input("Enter the no of hours : "))
     charge=time*10
-    import random
     place = print("You have been alloted space at block",random.randint(1,100),"for vehicle",reg_no,"upto",time,"hours @",charge,"₹")
 
 elif  veh == "B":
     reg_no=input("Enter the Reg no : ")
     time=int(input("Enter the no of hours : "))
-    print("Money to be payed",time*15)
+    charge=time*15
     place = print("You have been alloted space at block",random.randint(1,100),"for vehicle",reg_no,"upto",time,"hours @",charge,"₹")
-elif veh == "c":
+elif veh == "C":
     reg_no=input("Enter the Reg no : ")
     time=int(input("Enter the no of hours : "))
-    print("Money to be payed",time*20)
+    charge=time*20
     place = print("You have been alloted space at block",random.randint(1,100),"for vehicle",reg_no,"upto",time,"hours @",charge,"₹")
+else:
+    print("Parking unavailable")
