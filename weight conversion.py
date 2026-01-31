479565418bf4185b8ec426a8a0f1cf1059bc02ed
Subhash <kos720500@gmail.com>
2026-01-31 13:09:59 +0530

    weight conversion

--- weight conversion.py
+++ weight conversion.py
@@ -1,11 +1,11 @@
-a = int(input('''choose the option :
-               1. kilograms to pound
-               2. Pound to kilograms'''))
+a = int(input('choose the option :\n 1. kilograms to pound\n 2. Pound to kilograms\n'))
 if a == 1:
     kg=float(input("Enter the weight in kg : "))
     pound = kg*2.2
-    print(pound)
-else:
+    print(kg,"kg to",pound,"lb")
+elif a==2:
     pound=float(input("Enter the weight in pounds : "))
     kg = pound*0.45
-    print(kg)
+    print(pound,"lb","to",kg,"kg")
+else:
+    print("conversion not found!")
