6e0b4d1c9b7e525dbfaa45d958978eb0a9f0425b
Subhash <kos720500@gmail.com>
2026-02-01 17:23:59 +0530

    prime or not

--- parking ticket.py
+++ parking ticket.py
@@ -8,7 +8,6 @@ if veh == "A":
     time=int(input("Enter the no of hours : "))
     charge=time*10
     place = print("You have been alloted space at block",random.randint(1,100),"for vehicle",reg_no,"upto",time,"hours @",charge,"₹")
-
 elif  veh == "B":
     reg_no=input("Enter the Reg no : ")
     time=int(input("Enter the no of hours : "))
---
+++ prime or not.py
@@ -0,0 +1,13 @@
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print("Not Prime")
+else:
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count = count + 1
+    if count == 2:
+        print("Prime")
+    else:
+        print("Not Prime")
