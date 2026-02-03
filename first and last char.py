aef301e4fa0dcd4395fd9edba22ec7b2a05a976d
Subhash <kos720500@gmail.com>
2026-02-03 08:53:49 +0530

    first and last char

--- first and last char.py
+++ first and last char.py
@@ -1,6 +1,18 @@
 greet = "PUSHPA" #positive indexing
 print(greet[0])
 print(greet[5])
+
 name = "joshua"
 print(name[0])
 print(name[5])
+
+n=(input('Enter a number = '))
+print(n[0])
+print(n[-1])
+
+num=int(input('Enter number : '))
+ld = num%10
+while num>10:
+    num=num//10
+print("First character : ",num)
+print("Last character : ",ld)
