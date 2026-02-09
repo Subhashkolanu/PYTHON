10a0ba58f049111a7dc0f76c5ca53b5bce79de4b
Subhash <kos720500@gmail.com>
2026-02-09 21:40:39 +0530

    password gen v2

---
+++ password gen v2.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+length = 8
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(length):
+    password += random.choice(characters)
+
+print("Suggested password:", password)
