cfe399efeaa52f80d2bba9397f76ba641e6436d6
Subhash <kos720500@gmail.com>
2026-02-16 21:33:49 +0530

    rock paper sccissors

--- rock paper sccissors.py
+++ rock paper sccissors.py
@@ -1,15 +1,15 @@
 import random
-c=['rock','paper','scissors']
+c=['rock','paper','scisors']
 cg=random.choice(c)
 p=input('Enter input :')
 if cg==p:
     print('Its a TIE')
 elif p=='paper' and cg=='rock':
     print('Yay! you win')
-elif p=='scissor'and cg=='paper':
+elif p=='scisors'and cg=='paper':
     print('Yay! you win')
-elif p=='rock' and cg=='paper':
+elif p=='rock' and cg=='scisors':
     print('Yay! you win')
 else:
-    print('Game over!')
+    print('Game over! You lost...')
 print(cg)
