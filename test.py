8dfa419d8385a54ac00739e7cb4d291622ad48a6
Subhash <kos720500@gmail.com>
2026-02-20 20:00:42 +0530

    palindrome v3

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 print("This file is created to test Git and Github synchronization.")
 print("Make some changes to see if they are reflected on GitHub.")
 print("Adding another line to test synchronization.")
+
+import pyglet
+print(pyglet.version)
