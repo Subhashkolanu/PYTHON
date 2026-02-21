faa44d4b9c62fbf02acaa2cc79a8c9d2df7c1f87
Subhash <kos720500@gmail.com>
2026-02-21 17:42:57 +0530

    Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,31 +1,32 @@
 a = int(input("Enter the value of a :"))
 b = int(input("Enter the value of b :"))
-add = a+b
-subtraction = a-b
-multplication = a*b
-if b!=0:
-    real_division = a/b
-    floor_division = a//b
-exponent  = a**b
 print("Choose your operation : +,-,*,/,//,**")
 op = input("Enter your choice : ")
 if op == "+":
+    add = a+b
     print("Addition = ",add)
+    
 elif op == "-":
+    subtraction = a-b
     print("subtraction = ",subtraction)
 elif op == "*":
+    multplication = a*b
     print("multiplication = ",multplication)
 elif op == "/":
     if b!=0:
+        if b!=0:
+            real_division = a/b
         print("Real division = ",real_division)
     else:
         print("Division is not possible")
 elif op == "//":
     if b!=0:
+        floor_division = a//b
         print("Floor division = ",floor_division)
     else:
      print("Division is not possible")
 elif op == "**":
+    exponent  = a**b
     print("Exponent = ",exponent)
 else:
     print("Invalid operation.")
