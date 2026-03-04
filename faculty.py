1fdf6e79a694e7ed540a78df1269174eab4147bd
Subhash <kos720500@gmail.com>
2026-03-04 20:30:23 +0530

    faculty

---
+++ faculty.py
@@ -0,0 +1,21 @@
+print('''python='p'
+Differential_equations_transforms='d'
+chemistry='c'
+dlsd='dl'
+eng='e'
+beee='b',''')
+f=input('Enter the subject name : ')
+if f=='p':
+    print('AVV')
+elif f=='d':
+    print('JD')
+elif f=='c':
+    print('RK')
+elif f=='dl':
+    print('VK')
+elif f=='e':
+    print('PS')
+elif f=='b':
+    print('LK')
+else:
+    print('Subject not found')
--- voting.py
+++
@@ -1,5 +0,0 @@
-A = int(input("Enter a positive integer : "))
-if A>=18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
