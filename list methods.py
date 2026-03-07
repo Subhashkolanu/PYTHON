dabeefe5e98001a4afacf7194a94bda4fc512859
Subhash <kos720500@gmail.com>
2026-03-07 21:58:38 +0530

    list methods

--- list methods.py
+++ list methods.py
@@ -1,3 +1,14 @@
 l=['Java','Python','C','C++','c#']
 x=['BMW','AUDI','TOYOTA','FIAT','LEXUS']
-print(x.append("Benz"))
+n = ["sai", "rahul", "anil", "pavan", "kiran"]
+m = [78, 92, 65, 88, 74, 91]
+print("Original language list : ",l)
+print("Original cars list : ",x)
+print("Original names list : ",n)
+print("Original integers list : ",m)
+x.append("BENZ")
+print("Method append : ",x)
+m.sort()
+print("Method sort : ",m)
+n.insert(5,"subhash")
+print("Method insert : ",n)
