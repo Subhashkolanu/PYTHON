8309398a66ccaa74acea8b98ce6e3a7445c1fe60
Subhash <kos720500@gmail.com>
2026-03-08 17:59:45 +0530

    list methods

--- list methods.py
+++ list methods.py
@@ -2,6 +2,7 @@ l=['Java','Python','C','C++','c#']
 x=['BMW','AUDI','TOYOTA','FIAT','LEXUS']
 n = ["sai", "rahul", "anil", "pavan", "kiran"]
 m = [78, 92, 65, 88, 74, 91]
+c=[]
 print("Original language list : ",l)
 print("Original cars list : ",x)
 print("Original names list : ",n)
@@ -10,6 +11,8 @@ x.append("BENZ")
 print("Method append : ",x)
 m.sort()
 print("Method sort : ",m)
+m.reverse()
+print("Method reverse : ",m)
 n.insert(5,"subhash")
 print("Method insert : ",n)
 n.extend(m)
@@ -17,3 +20,9 @@ print("Method extend : ",n)
 n.remove("rahul")
 m.remove(88)
 print("Method remove : ",n,m)
+c=x.copy()
+print("Method copy : ",c)
+c.clear()
+print("Method clear : ",c)
+ca=x.count("BENZ")
+print("Method count : ",ca)
