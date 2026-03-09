c169356757eb07f7591a1c346aef4024a6e32517
Subhash <kos720500@gmail.com>
2026-03-09 20:49:34 +0530

    basic func

--- basic func.py
+++ basic func.py
@@ -7,3 +7,8 @@ def cap(s):
     return s.capitalize()
 s=input("data: ")
 print("s :",cap(s))
+
+def count(s):
+    return s.count('@')
+s=input("Enter your mail id : ")
+print("Count of @ in str : ",count(s))
