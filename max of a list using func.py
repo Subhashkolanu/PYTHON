b0ef5fd92f75d28f367ebc60e63cd00b101d6cb8
Subhash <kos720500@gmail.com>
2026-03-12 14:41:57 +0530

    max of as string using func

---
+++ max of a list using func.py
@@ -0,0 +1,6 @@
+def max_list(lt):
+    max(lt)
+    print("Max in list : ",max(lt))
+    return
+l=input("data : ").split(",")
+max_list(l)
---
+++ reverse string using func.py
@@ -0,0 +1,6 @@
+def rev(str):
+    str=str[::-1]
+    print("Reversed string using functions : ",str)
+    return
+s=input("Enter string : ")
+rev(s)
