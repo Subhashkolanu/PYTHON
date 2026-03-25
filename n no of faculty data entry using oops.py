5e0b9a59655cfd42a2504e3f48e30d06a99d9284
Subhash <kos720500@gmail.com>
2026-03-25 13:00:24 +0530

    for range update

--- n no of faculty data entry using oops.py
+++ n no of faculty data entry using oops.py
@@ -1,6 +1,4 @@
 class fac:
-    n=int(input("Enter no of faculty : "))
-    for i in range(n):
         def __init__(self,name,rn,exp):
             self.nm=name
             self.rid=rn
@@ -9,8 +7,10 @@ class fac:
             print("Name : ",self.nm)
             print("Register number : ",self.rid)
             print("Experience : ",self.xp)
-n=input("Enter name : ")
-u=int(input("Enter register number : "))
-e=int(input("Enter experience : "))
-f=fac(n,u,e)
-f.det()
+n=int(input("Enter no of faculty : "))
+for i in range(n):
+    n=input("Enter name : ")
+    u=int(input("Enter register number : "))
+    e=int(input("Enter experience : "))
+    f=fac(n,u,e)
+    f.det()
