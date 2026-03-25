3e876a5e08ab2c55612268e323082b6a57e0c8fa
Subhash <kos720500@gmail.com>
2026-03-25 12:22:37 +0530

    Faculty data collection using oops

---
+++ faculty data collection using oops.py
@@ -0,0 +1,14 @@
+class fac:
+    def __init__(self,name,rn,exp):
+        self.nm=name
+        self.rid=rn
+        self.xp=exp
+    def det(self):
+        print("Name : ",self.nm)
+        print("Register number : ",self.rid)
+        print("Experience : ",self.xp)
+n=input("Enter name : ")
+u=int(input("Enter register number : "))
+e=int(input("Enter experience : "))
+f=fac(n,u,e)
+f.det()
