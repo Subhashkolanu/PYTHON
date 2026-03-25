90c61df62a4ba57ec7f5d1ed907ce6e4cc6a05f7
Subhash <kos720500@gmail.com>
2026-03-25 12:45:15 +0530

    Student result

---
+++ student result sheet.py
@@ -0,0 +1,17 @@
+class student:
+    def __init__(self,roll_number,name,ap,res):
+        self.rn=roll_number
+        self.nm=name
+        self.app=ap
+        self.r=res
+    def getdet(self):
+        print("Name : ",self.nm)
+        print("Roll number : ",self.rn)
+        print("Attendance : ",self.app)
+        print("CGPA/SGPA : ",self.r)
+n=input("Enter name : ")
+u=int(input("Enter roll number : "))
+e=float(input("Attendance : "))
+#c=float(input("CGPA/SGPA : "))
+s=student(n,u,e,float(input("Enter cgpa:")))
+s.getdet()
