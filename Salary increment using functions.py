117b55add03db7801b389b12198d929b5536bded
Subhash <kos720500@gmail.com>
2026-03-29 10:33:07 +0530

    Salary slips using functions

--- Salary increment using functions.py
+++ Salary increment using functions.py
@@ -4,15 +4,21 @@ e=int(input("Experience : "))
 bo=0
 def experience():
     if e>5:
-        bo=b*(20/100)
+        #bo=b*(20/100)
         print("Bonus : ",bo)
     else:
         bo=b*(10/100)
-        print("Bonus : ",bo)
+        #print("Bonus : ",bo)
     return bo
 bonus=experience()
 def total():
     t=b+bonus
-    print("Total salary : ",t)
-    return
+    #print("Total salary : ",t)
+    return t
 total()
+print("-----Salary slip-----")
+print("Name :",n)
+print("Basic salary :",b)
+print("Experience :",e)
+print("Bonus :",experience())
+print("Total salary :",total())
