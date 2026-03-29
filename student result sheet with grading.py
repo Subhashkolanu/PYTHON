a04e69d245322c26adadb1a051884100b41e654d
Subhash <kos720500@gmail.com>
2026-03-29 09:49:02 +0530

    Student result sheet with oops

--- student result sheet with grading.py
+++ student result sheet with grading.py
@@ -9,6 +9,22 @@ class student:
         print("Roll number : ",self.rn)
         print("Attendance : ",self.app)
         print("CGPA/SGPA : ",self.r)
+    def grade(self):
+        if self.r >= 7.0:
+            print("Grade A")
+            print("Promoted")
+        elif self.r >= 6.5:
+            print("Grade B")
+            print("Promoted")
+        elif self.r >= 6.0:
+            print("Grade C")
+            print("Promoted")
+        elif self.r >= 5.5:
+            print("Grade D")
+            print("Promoted")
+        else:
+            print("Failed")
+
 n=input("Enter name : ")
 u=int(input("Enter roll number : "))
 e=float(input("Enter attendance percentage : "))
@@ -16,19 +32,4 @@ c=float(input("CGPA/SGPA : "))
 s=student(u,n,e,c)
 print("-----Result sheet-----")
 s.getdet()
-if c>7.0:
-    print("GRADE A")
-    print("Promoted to next semester")
-elif c>6.5 and c<7.0:
-    print("GRADE B")
-    print("Promoted to next semester")
-elif c>6.0 and c<6.5:
-    print("GRADE c")
-    print("Promoted to next semester")
-elif c>5.5 and c<6.0:
-    print("GRADE D")
-    print("Promoted to next semester")
-else:
-    print("Failed")
-if e<75:
-    print("Result delared with Fine for attendance")
+s.grade()
