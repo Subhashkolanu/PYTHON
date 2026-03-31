003d537f588c4a1293250a3d6cf18e4861e58dd5
Subhash <kos720500@gmail.com>
2026-03-31 19:15:38 +0530

    Total Bill using func with discount

---
+++ total bill with discount using functions.py
@@ -0,0 +1,19 @@
+def total_amt(price,quantity,discount=0):
+    total=price*quantity
+    discount=total*discount/100
+    final_amt=total-discount
+    print("-----Bill reciept-----")
+    if final_amt==total:
+        print("Total amount :",total)
+        print("Final amount :",final_amt)
+    else:
+        print("Total amount :",total)   
+    if discount>0:
+        print("Discount :",discount)
+        print("Final ammount :",final_amt)
+    else:
+        print("")
+p=float(input("Enter the price : "))
+q=int(input("Enter the quantity : "))
+total_amt(p,q)
+total_amt(p,q,discount=10)
