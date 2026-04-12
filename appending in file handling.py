71bb0d80b06ae82c45a9fdfbd8e8855890ff58e0
Subhash <kos720500@gmail.com>
2026-04-12 13:33:39 +0530

    Appending in file handling

---
+++ appending in file handling.py
@@ -0,0 +1,3 @@
+file=open("mydata","a")
+file.write("Date of birth : 30/10/2007")
+file.close
--- editing existing file using file handling.py
+++ editing existing file using file handling.py
@@ -2,6 +2,6 @@ file=open("mydata.txt","r")
 n=int(input("Enter no of lines : "))
 if n<=5:
      for i in range(n):
-          print(file.readline())
+          print(file.readline(),end="")
 else:
      print("Exceeded the value.")
