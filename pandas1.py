a1321de817f86a6ee6dcd4b03dc91ae144be71a1
Subhash <kos720500@gmail.com>
2026-04-15 12:51:46 +0530

    Pamdas1

--- pandas1.py
+++ pandas1.py
@@ -1,7 +1,10 @@
 import pandas as s
-list1=s.Series([10,20,30,40,"KSS"]) #object 
-list2=s.Series("name:SK") #str
-list3=s.Series([0.5,0.2,0.9])
-print(list1)
-print(list2)
-print(list3)
+l1=s.Series([10,20,30,40,"KSS"]) #object 
+l2=s.Series("say my name") #str
+l3=s.Series([0.5,0.2,0.9]) #float
+print(l1)
+print(l2)
+print(l3)
+print("Values of list 1 :",l1.values)
+print("Values of list 2 :",l2.values)
+print("Values of list 3 :",l3.values)
