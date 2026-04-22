a8fa0dc6fa1193e86208e3957b228ce61f3c0e7f
Subhash <kos720500@gmail.com>
2026-04-22 18:33:30 +0530

    Pandas datafram display2

--- pandas dataframe display2.py
+++ pandas dataframe display2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 record=[
-    {"Name":"Bharat","Age":17,"Marks":80},
-     {"Name":"Subhash","Age":18,"Marks":99}
+    {"Name":"Bharat","Age":17,"Marks":99},
+     {"Name":" vardhan","Age":18,"Marks":99}
 ]
 d=pd.DataFrame(record)
 print(d)
