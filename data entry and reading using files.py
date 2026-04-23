c1e295609208a9a756e4deee42054d49823e621e
Subhash <kos720500@gmail.com>
2026-04-23 21:25:07 +0530

    Data Entry

--- data entry and reading using files.py
+++ data entry and reading using files.py
@@ -2,4 +2,5 @@ name=["Bharat","Shanmukha","Venkatesh","Anish"]
 with open("newfile.txt","w") as f:
     f.writelines(name)
 with open("newfile.txt","r") as file:
-    file.readlines
+    for line in file:
+        print(line.strip())
