91e43b8ee9f92e372343a224667b48782dfea9a3
Subhash <kos720500@gmail.com>
2026-04-26 16:40:26 +0530

    Reading images

--- Reading images.py
+++ Reading images.py
@@ -2,5 +2,6 @@ with open ("Brosan.jpg","rb") as img:
     image=img.read()
 with open ("Image.jpg","wb") as file:
     file.write(image)
+print("Image data copied succesfully")
 import os
 os.startfile("Image.jpg")
