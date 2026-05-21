61daa8042c09d6b385b6d1b565c42a34f29d81ae
Subhash <kos720500@gmail.com>
2026-05-21 19:38:34 +0530

    Private member oops

--- private member oops.py
+++ private member oops.py
@@ -1,8 +1,8 @@
 class A:
     def __init__(self,Input):
-        self._x=Input
+        self.__x=Input
     def display(self):
-        print("Value of x :",self._x)
+        print("Value of x :",self.__x)
 n=int(input("Enter value : "))
 a=A(n)
 a.display()
