32a6ee4bdd839cb5b23992d0c82f0eae6e9b8e86
Subhash <kos720500@gmail.com>
2026-05-25 13:32:09 +0530

    Copying content using files

--- Copying content using files.py
+++ Copying content using files.py
@@ -1,8 +1,8 @@
 #copying content from one file to another
 with open("Source.txt","r") as src:
     content=src.read()
-with open ("destination.txt","w") as dist:
-    copy=dist.write(content)
+with open ("destination.txt","w") as dest:
+    copy=dest.write(content)
 print("Content copied succesfully!")
 import os
 os.startfile("destination.txt")
