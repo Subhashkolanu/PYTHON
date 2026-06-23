38ea7bdd84a6afa2ca4491c3d75f239624a1b342
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-06-23 09:25:14 +0530

    isinstance test

---
+++ isinstance.py
@@ -0,0 +1,8 @@
+Name="Sri krishna"
+Age=29
+Salary=59000.15
+Married=True
+Family={"Vasudev","Yashoda","Balaram"}
+Family_dict={"Father":"Vasudev","Mother":"Yashoda","Brother":"Balaram"}
+Wife=("Rukmini","Satyabhama","Jambavathi","Kalida","Mithravinda","Lakshmana","Bhadra","Nagnajiti")
+Enemy=["Sisupaludu","Dantrvatrulu",]
