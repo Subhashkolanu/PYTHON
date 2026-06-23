df96cc430f2b2b54036509a9a6756f4e6ee59f1c
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-06-23 09:52:23 +0530

    isinstance test

--- isinstance.py
+++ isinstance.py
@@ -1,8 +1,24 @@
 Name="Sri krishna"
+print("Name :",Name)
+print("isinstance: String",isinstance(Name,str))
 Age=29
+print("Age :",Age)
+print("isinstance: integer",isinstance(Age,int))
 Salary=59000.15
+print("Salary :",Salary)
+print("isinstance: float",isinstance(Salary,float))
 Married=True
+print("Maritial status :",Married)
+print("isinstance: Boolean",isinstance(Married,bool))
 Family={"Vasudev","Yashoda","Balaram"}
+print("Family :",Family)
+print("isinstance: Set",isinstance(Family,set))
 Family_dict={"Father":"Vasudev","Mother":"Yashoda","Brother":"Balaram"}
+print("Family members :",Family_dict)
+print("isinstance: dict",isinstance(Family_dict,dict))
 Wife=("Rukmini","Satyabhama","Jambavathi","Kalida","Mithravinda","Lakshmana","Bhadra","Nagnajiti")
-Enemy=["Sisupaludu","Dantrvatrulu",]
+print("Wives :",Wife)
+print("isinstance: Tuple",isinstance(Wife,tuple))
+Enemy=["Sisupaludu","Dantrvatrulu","Kamsa","Putana"]
+print("Enimies :",Enemy)
+print("isinstance: List",isinstance(Enemy,list))
