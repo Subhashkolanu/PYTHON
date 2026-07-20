142770308bd328820ce62966890e23ef4afbfcae
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-20 16:23:38 +0530

    admision form

--- admisson form.py
+++ admisson form.py
@@ -1,7 +1,6 @@
 name = input("Enter your name : ")
 print("\nHello", name, "! Welcome to VVITU")
-
-int_marks = int(input("Enter your INTERMEDIATE marks : "))
+int_marks = int(input("\nEnter your INTERMEDIATE marks : "))
 jee_per = int(input("Enter your JEE percentile : "))
 
 # checking eligibility
@@ -10,95 +9,102 @@ if int_marks >= 750 or jee_per >= 80:
     admission = "Y"
     merit="Y"
 else:
-    print("YOU ARE NOT ELIGIBLE FOR ADMISSION :(")
+    print("\nYOU ARE NOT ELIGIBLE FOR ADMISSION :(")
     merit="N"
-    admission = input("Enter your choice Y/N : ").upper()
+    #Mangement quota
+    admission = input("\nEnter your choice (MANAGEMENT) Y/N : ").upper()
     if admission == "Y":
-        print("\nThese are the courses offered here")
+        print("These are the courses offered here")
         print("1. CSE")
         print("2. ECE")
         print("3. EEE")
         print("4. ME")
         print("5. CE")
+        branch = input("Choose branch : ").upper()
+        if branch == "CSE":
+            s=input("Specialisation : Y/n : ").upper()
+            if s=="Y":
+                print("Specialisations available:")
+                print("1. AI-ML")
+                print("2. AI-DS")
+                print("3. CYBER SECURITY")
+                spec = input("Choose your specialisation : ").upper()
+                if spec == "AI-ML":
+                    print("\nSeat has been allotted in CSE-AI-ML")
+                    print("Fee per year = ₹180000")
 
-    branch = input("\nChoose branch : ").upper()
+                elif spec == "AI-DS":
+                    print("\nSeat has been allotted in CSE-AI-DS")
+                    print("Fee per year = ₹140000")
 
-    if branch == "CSE":
-        print("\nYOU HAVE CHOSEN CSE")
-        print("Specialisations available:")
-        print("1. AI-ML")
-        print("2. AI-DS")
-        print("3. CYBER SECURITY")
+                elif spec == "CYBER SECURITY":
+                    print("\nSeat has been allotted in CSE-CYBER SECURITY")
+                    print("Fee per year = ₹150000")
 
-        spec = input("Choose your specialisation : ").upper()
+                else:
+                    print("INVALID SPECIALISATION")
+            else:
+                print("YOU HAVE CHOOSEN CSE")
+                print("\nSeat has been allotted in CSE-CORE")
+                print("Fee per year = ₹135000")
 
-        if spec == "AI-ML":
-            print("\nSeat has been allotted in CSE-AI-ML")
-            print("Fee per year = ₹180000")
+        elif branch == "ECE":
+            print("\nYOU HAVE CHOSEN ECE")
+            print("Fee per year = ₹130000")
 
-        elif spec == "AI-DS":
-            print("\nSeat has been allotted in CSE-AI-DS")
-            print("Fee per year = ₹140000")
+        elif branch == "EEE":
+            print("\nYOU HAVE CHOOSEN EEE")
+            print("Fee per year = ₹110000")
 
-        elif spec == "CYBER SECURITY":
-            print("\nSeat has been allotted in CSE-CYBER SECURITY")
-            print("Fee per year = ₹150000")
+        elif branch == "ME":
+            print("\nYOU HAVE CHOOSEN MECHANICAL ENGINEERING")
+            print("Fee per year = ₹110000")
+
+        elif branch == "CE":
+            print("\nYOU HAVE CHOOSEN CIVIL ENGINEERING")
+            print("Fee per year = ₹100000")
 
         else:
-            print("INVALID SPECIALISATION")
-
-    elif branch == "ECE":
-        print("\nYOU HAVE CHOSEN ECE")
-        print("Fee per year = ₹130000")
-
-    elif branch == "EEE":
-        print("\nYOU HAVE CHOSEN EEE")
-        print("Fee per year = ₹110000")
-
-    elif branch == "ME":
-        print("\nYOU HAVE CHOOSEN MECHANICAL ENGINEERING")
-        print("Fee per year = ₹110000")
-
-    elif branch == "CE":
-        print("\nYOU HAVE CHOOSEN CIVIL ENGINEERING")
-        print("Fee per year = ₹100000")
-
-    else:
-        print("INVALID BRANCH")
-
+            print("INVALID BRANCH")
+#Merit quota
 if admission == "Y" and merit == "Y":
-    print("\nThese are the courses offered here")
+    print("These are the courses offered here")
     print("1. CSE")
     print("2. ECE")
     print("3. EEE")
     print("4. ME")
     print("5. CE")
 
-    branch = input("\nChoose branch : ").upper()
+    branch = input("Choose branch : ").upper()
 
     if branch == "CSE":
-        print("\nYOU HAVE CHOSEN CSE")
-        print("Specialisations available:")
-        print("1. AI-ML")
-        print("2. AI-DS")
-        print("3. CYBER SECURITY")
+        s=input("Specialisation : Y/n : ").upper()
+        if s=="Y":
+            print("Specialisations available:")
+            print("1. AI-ML")
+            print("2. AI-DS")
+            print("3. CYBER SECURITY")
 
-        spec = input("Choose your specialisation : ").upper()
+            spec = input("Choose your specialisation : ").upper()
 
-        if spec == "AI-ML":
-            print("\nSeat has been allotted in CSE-AI-ML")
-            print("Fee per year = ₹130000")
+            if spec == "AI-ML":
+                print("\nSeat has been allotted in CSE-AI-ML")
+                print("Fee per year = ₹130000")
 
-        elif spec == "AI-DS":
-            print("\nSeat has been allotted in CSE-AI-DS")
-            print("Fee per year = ₹128000")
+            elif spec == "AI-DS":
+                print("\nSeat has been allotted in CSE-AI-DS")
+                print("Fee per year = ₹128000")
 
-        elif spec == "CYBER SECURITY":
-            print("\nSeat has been allotted in CSE-CYBER SECURITY")
-            print("Fee per year = ₹135000")
+            elif spec == "CYBER SECURITY":
+                print("\nSeat has been allotted in CSE-CYBER SECURITY")
+                print("Fee per year = ₹135000")
 
+            else:
+                print("INVALID SPECIALISATION")
         else:
-            print("INVALID SPECIALISATION")
+            print("\nSeat has been allotted in CSE-CORE")
+            print("Fee per year = ₹125000")
+
 
     elif branch == "ECE":
         print("\nYOU HAVE CHOSEN ECE")
