5c9984eb58971c299918963d1c80766d47bfa537
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-20 19:42:31 +0530

    movie ticket

--- movie ticket.py
+++ movie ticket.py
@@ -1,9 +1,9 @@
 age=int(input("Enter age : "))
 if age>=5:
-    seat=input("Seats available (True/False) : ")
-    if seat == "True" or seat == "true":
-        payment=(input("Payment status : "))
-        if payment=="True" or payment == "true":
+    seat=input("Seats available (True/False) : ").capitalize()
+    if seat == "True":
+        payment=(input("Payment status : ")).capitalize()
+        if payment=="True":
             print("Ticket booked succesfully")
         else:
             print("Payment failed")
