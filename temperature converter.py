6cd05b5f8938964114c6460b6587c0d9c524df4b
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-22 06:32:22 +0530

    Temperature converter

--- temperature converter.py
+++ temperature converter.py
@@ -1,5 +1,7 @@
 temp = int(input("Enter temperature : "))
 unit = input("Enter unit (C/F) : ").upper()
 
-if unit == "F":
-    print(f"{temp} c ")
+if unit == "C":
+    print(f"{temp}°C to {round((temp*9/5)+32,1)}°F")
+elif unit == "F":
+    print(f"{temp}°F to {round((temp-32)*5/9,1)}°C")
