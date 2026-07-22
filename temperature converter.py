9fd8d53ff90a060b35189784a586b322abc0100f
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-22 06:20:27 +0530

    weight conversion

---
+++ temperature converter.py
@@ -0,0 +1,5 @@
+temp = int(input("Enter temperature : "))
+unit = input("Enter unit (C/F) : ").upper()
+
+if unit == "F":
+    print(f"{temp} c ")
--- weight conversion.py
+++ weight conversion.py
@@ -1,8 +1,8 @@
 weight = float(input('Enter your weight : '))
 unit = input("Kilograms or pounds (K or L) : ").upper()
 if unit == "K":
-    print(f"{weight} kg to pounds {round(weight*2.205,2)} lbs")
+    print(f"{weight} kg converted to {round(weight*2.205,2)} lbs")
 elif unit=="L":
-    print(f"{weight} pounds to kg {round(weight/2.205,2)} kg's")
+    print(f"{weight} pounds converted to {round(weight/2.205,2)} kg's")
 else:
     print("conversion not found!")
